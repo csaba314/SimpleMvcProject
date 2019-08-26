742274f78f2964bd1ea86585df5600950d7fe7be
Csaba Erdes <csaba.erdes@gmail.com>
2019-08-26 17:12:01 +0200

    add VehicleService class

---
+++ Project.Service/Services/IVehicleService.cs
@@ -0,0 +1,33 @@
+﻿using Project.Service.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Service.Services
+{
+    public interface IVehicleService
+    {
+        #region Vehicle Make
+        VehicleMake GetVehicleMake(int id);
+        IEnumerable<VehicleMake> GetAllVehicleMake();
+
+        void AddVehicleMake(VehicleMake make);
+
+        void RemoveVehicleMake(VehicleMake make);
+        #endregion
+
+
+        #region Vehicle Model
+        VehicleModel GetVehicleModel(int id);
+        IEnumerable<VehicleModel> GetAllVehicleModels();
+
+        void AddVehicleModel(VehicleModel model);
+
+        void RemoveVehicleModel(VehicleModel model);
+        #endregion
+
+        int SaveChanges();
+    }
+}
---
+++ Project.Service/Services/VehicleService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Service.Model;
+
+namespace Project.Service.Services
+{
+    public class VehicleService : IVehicleService
+    {
+        private ProjectDbContext _context;
+
+        public VehicleService()
+        {
+            _context = ProjectDbContext.GetDbContext();
+        }
+
+
+        #region Vehicle Make
+        public VehicleMake GetVehicleMake(int id)
+        {
+            return _context.VehicleMakes.Find(id);
+        }
+
+        public IEnumerable<VehicleMake> GetAllVehicleMake()
+        {
+            return _context.VehicleMakes;
+        }
+
+        public void AddVehicleMake(VehicleMake make)
+        {
+            _context.VehicleMakes.Add(make);
+        }
+
+        public void RemoveVehicleMake(VehicleMake make)
+        {
+            _context.VehicleMakes.Remove(make);
+        }
+        #endregion
+
+
+
+
+        #region Vehicle Model
+        public VehicleModel GetVehicleModel(int id)
+        {
+            return _context.VehicleModels.Find(id);
+        }
+
+        public IEnumerable<VehicleModel> GetAllVehicleModels()
+        {
+            return _context.VehicleModels;
+        }
+
+        public void AddVehicleModel(VehicleModel model)
+        {
+            _context.VehicleModels.Add(model);
+        }
+
+        public void RemoveVehicleModel(VehicleModel model)
+        {
+            _context.VehicleModels.Remove(model);
+        }
+        #endregion
+
+
+
+        public int SaveChanges()
+        {
+            return _context.SaveChanges();
+        }
+    }
+}
